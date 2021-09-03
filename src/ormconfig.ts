c11ac38be692a54d576dbff2762e26080066ca0c
Arseniy Suschevskiy <a.suschevskiy@voiso.com>
2021-09-03 21:52:37 +0300

    Add typeorm

--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -6,7 +6,9 @@ const config: ConnectionOptions = {
   port: 5432,
   username: "mediumclone",
   password: "fifa2008",
-  database: "mediumclone"
+  database: "mediumclone",
+  entities: [__dirname + "/**/*.entity{.ts,.js}"],
+  synchronize: true
 }
 
 export default config
