ce4f569228427c4ae9c81e2866185f6930b2ae57
Arseniy Suschevskiy <a.suschevskiy@voiso.com>
2021-09-04 17:03:34 +0300

    Add config service and .env for typeorm configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,11 @@ import { TypeOrmModule } from "@nestjs/typeorm"
 import { AppController } from "@app/app.controller"
 import { AppService } from "@app/app.service"
 import { TagModule } from "@app/tag/tag.module"
-import ormconfig from "@app/ormconfig"
 import { UserModule } from "@app/user/user.module"
+import { configService } from "./config.service"
 
 @Module({
-  imports: [TypeOrmModule.forRoot(ormconfig), TagModule, UserModule],
+  imports: [TypeOrmModule.forRoot(configService.getTypeOrmConfig()), TagModule, UserModule],
   controllers: [AppController],
   providers: [AppService]
 })
